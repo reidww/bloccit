d8caf5db30b7913b663389148356878057b1314d
Reid <reidww@gmail.com>
2014-12-15 12:09:51 +0000

    Associated users with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,21 @@
 require 'faker'
 
+# Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+
 #Create Posts
 50.times do 
   Post.create!(
+    user:    users.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
     )
@@ -12,10 +25,18 @@ posts = Post.all
 # Create Comments
 100.times do
   Comment.create!(
+    # user: users.sample, # we have not yet associated Users with Comments
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
 end
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'reidww@gmail.com',
+  password: 'helloworld'
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
